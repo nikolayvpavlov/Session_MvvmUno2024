d88fa0d8d92029ad3e3305c754ff3ce229e6c5f5
Nikolay Pavlov <nikolay.pavlov@framestory.org>
2024-11-18 17:05:01 +0200

    Enrich the DM a bit.

--- AcmeServices/Models/AcmeContext.cs
+++ AcmeServices/Models/AcmeContext.cs
@@ -16,11 +16,11 @@ namespace AcmeServices.Models
         {
             inMemoryRelations = new List<Relation>()
             {
-                new Relation() { Id = 1, Name = "Bright Roads Company Ltd.", Country = "USA" },                
-                new Relation() { Id = 2, Name = "Slinky Services Co.", Country = "USA"},
-                new Relation() { Id = 3, Name = "Naughty Nutgoblins Inc.", Country = "UK"},
-                new Relation() { Id = 4, Name = "Derriér Inc.", Country = "France"},
-                new Relation() { Id = 5, Name = "Bright Future Railroads", Country = "Bulgaria" },
+                new Relation() { Id = 1, Name = "Bright Roads Company Ltd.", Country = "USA", Phone="+1 555 123456", ContactPerson="John Doe", },                
+                new Relation() { Id = 2, Name = "Slinky Services Co.", Country = "USA", Phone="+1 555 3636999", ContactPerson="Jane Doe", },
+                new Relation() { Id = 3, Name = "Naughty Nutgoblins Inc.", Country = "UK", Phone="+44 999 123456", ContactPerson="Ford Prefekt", },
+                new Relation() { Id = 4, Name = "Derriér Inc.", Country = "France", Phone="+33 000 123456", ContactPerson="Loui De Fennêtre", },
+                new Relation() { Id = 5, Name = "Bright Future Railroads", Country = "Bulgaria", Phone="+359 999 123456", ContactPerson="Ivan Poluivanov",  },
             };
         }
 
--- AcmeServices/Models/Relation.cs
+++ AcmeServices/Models/Relation.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Country { get; set; } 
+        public string Country { get; set; }
+        public string Phone { get; set; }
+        public string ContactPerson { get; set; }
     }
 }
