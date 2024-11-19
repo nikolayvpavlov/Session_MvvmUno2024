45adc0b656d8e69b699815a0195a7ad7e0e9b19a
Nikolay Pavlov <nikolay.pavlov@framestory.org>
2024-11-19 17:39:06 +0200

    Draft Blazor app ready!

---
+++ AcmeBlazor/Components/Pages/Relations.razor.cs
@@ -0,0 +1,18 @@
+﻿using AcmeServices.ViewModels;
+using Microsoft.AspNetCore.Components;
+
+namespace AcmeBlazor.Components.Pages
+{
+    public partial class Relations
+    {
+        [Inject]
+        public RelationsSearchViewModel RelationsViewModel { get; set; }
+
+        private string SearchText { get; set; }
+
+        public async Task SearchRelationsAsync()
+        {
+            await RelationsViewModel.SearchDataAsync(SearchText);
+        }
+    }
+}
